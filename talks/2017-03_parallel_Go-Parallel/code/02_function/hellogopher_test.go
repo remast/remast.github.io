a3704b5c598e2bf37122083d6ba131f928cd4ac1
Jan Stamer <jan.stamer@gmail.com>
2020-03-14 10:06:15 +0100

    Updates

--- talks/2017-03_parallel_Go-Parallel/code/02_function/hellogopher_test.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "testing"
-
-func TestSayHelloGopher(t *testing.T) {
-	if HelloGopher() != "Hello Gopher!" {
-		t.Error("Unerwartetes Ergebnis.")
-	}
-}
--- talks/2017-03_parallel_Go-Parallel/images/array-slices.go
+++
@@ -1,7 +0,0 @@
-var a [2]string
-a[0] = "Hello"
-a[1] = "Gopher"
-fmt.Println(a[0] + " " + a[1])
-
-names := make([]string, 2)
-  names := []string{"Hans", "Franz"}
